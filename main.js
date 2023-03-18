925bd4d9a6a70a42c1a62d24dbdb91d5ed07a796
mashendragunawan <mashendragunawan04@gmail.com>
2023-03-18 20:57:34 +0700

    main

--- main.js
+++ main.js
@@ -4,8 +4,18 @@
 
 
 
-function jumlahkan(){
-    var a1 = document.getElementById('angka1').value;
-    var a2 = document.getElementById('angka2').value;
-    alert(a1+a2)
+function test(){
+    var nilaitinggi=100;    //var a1 = document.getElementById('angka1').value;
+   var nilai = document.getElementById('nilai').value;
+    if(nilaitinggi<70)
+    {
+        alert("nilai anda B");
+    }
+    else (nilaitinggi>=70&&nilaitinggi<100)
+    {
+        alert("nilai anda A");
+    }
+   
+    
+    
 }
