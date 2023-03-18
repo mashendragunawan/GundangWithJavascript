27d744784ceca45db082c86c473808d8a59ff967
mashendragunawan <mashendragunawan04@gmail.com>
2023-03-18 20:38:43 +0700

    berhasil

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-console.log('hello World')
+//console.log('hello World')
 
-console.log('aku sedang belajar node js')
+//console.log('aku sedang belajar node js')
+
+
+
+function jumlahkan(){
+    var a1 = document.getElementById('angka1').value;
+    var a2 = document.getElementById('angka2').value;
+    alert(a1+a2)
+}
