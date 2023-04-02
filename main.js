6204fca5c1d08154f814b55f896af561f28be078
mashendra <mashenendragunawan04@gmail.com>
2023-04-02 01:15:10 -0700

    main

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ console.log('mungkin cukup')
 console.log('mungkin cukup')
 console.log('mungkin cukup')
 console.log('asik laptop baru mas broo')
-
+console.log('aku juga sangat senang dengan semua hal yang ada pada laptop saya')
 
 
 
